b2e49b3140e126bec524c89787c0f56d32fcc9bf
Prodaaron <aaronmesfin60@gmail.com>
2025-01-24 20:56:18 +0300

    set up home, about and product page (Incomplete)

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,25 +1,29 @@
-import './App.css'
 import { BrowserRouter as Router, Routes, Route, NavLink } from "react-router-dom";
+import Home from '../pages/home.jsx';
+import Products from '../pages/products.jsx';
+import About from '../pages/about.jsx';
+import './App.css';
 
 function App() {
 
   return (
     <>
+
+      
       <Router>
+        
       <nav>
         <ul>
           <li><NavLink to="/" activeClassName="active">Home</NavLink></li>
+          <li><NavLink to="/products" activeClassName="active">Products</NavLink></li>
           <li><NavLink to="/about" activeClassName="active">About</NavLink></li>
-          <li><NavLink to="/contact" activeClassName="active">Contact</NavLink></li>
         </ul>
       </nav>
 
-
-        <Routes>
+      <Routes>
           <Route path="/" element={<Home />} />
-          <Route path="/home" element={<Home />} />
+          <Route path="/products" element={<Products />} />
           <Route path="/about" element={<About />} />
-          <Route path="/contact" element={<Contact />} />
         </Routes>
       </Router>
     </>
