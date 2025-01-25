bda2fcbeadee263822dc31038a17885ced9eabaa
Prodaaron <aaronmesfin60@gmail.com>
2025-01-25 10:58:50 +0300

    fixed header routing

--- client/components/header/header.jsx
+++ client/components/header/header.jsx
@@ -1,12 +1,7 @@
 import React, { useState, useEffect } from "react";
-import { BrowserRouter as Router, Routes, Route, NavLink } from "react-router-dom";
-import Home from "../../pages/home.jsx";
-import Products from "../../pages/products.jsx";
-import About from "../../pages/about.jsx";
-import Signup from "../../pages/signup.jsx";
-import Login from "../../pages/login.jsx";
 import Logo from "../../src/assets/images/logo/mesarg-logo-favico.ico";
 import "./header.css"
+import { NavLink } from "react-router-dom";
 
 const header = () => {
 
@@ -32,52 +27,44 @@ const header = () => {
 
   return (
     <div>
-      <Router>
-        <nav>
-            <div>
-                <img src={Logo} alt="logo" />
-            </div>
-            <ul>
-                <li>
-                    <NavLink to="/" activeClassName="active">Home</NavLink>
-                </li>
-                <li>
-                    <NavLink to="/products" activeClassName="active">Products</NavLink>
-                </li>
-                <li>
-                    <NavLink to="/about" activeClassName="active">About</NavLink>
-                </li>
-            </ul>
+      
+      <nav className="header-nav">
+        <div className="header-logo-container">
+          <img className="header-logo" src={Logo} alt="logo" />
+        </div>
+        <ul className="middle-header">
+          <li>
+            <NavLink to="/" activeClassName="active">Home</NavLink>
+          </li>
+          <li>
+            <NavLink to="/products" activeClassName="active">Products</NavLink>
+          </li>
+          <li>
+            <NavLink to="/about" activeClassName="active">About</NavLink>
+          </li>
+        </ul>
 
-            {!isLoggedIn ? (
-            <ul>
-                <li>
-                <NavLink to="/signup" activeClassName="active">Sign Up</NavLink>
-                </li>
-                <li>
-                <NavLink to="/login" activeClassName="active">Login</NavLink>
-                </li>
-                <li>
-                <button onClick={handleLogin}>Simulate Login</button>
-                </li>
-            </ul>
-            ) : (
-            <ul>
-                <li>
-                <button onClick={handleLogout}>Logout</button>
-                </li>
-            </ul>
-            )}
-        </nav>
-
-        <Routes>
-          <Route path="/" element={<Home />} />
-          <Route path="/products" element={<Products />} />
-          <Route path="/about" element={<About />} />
-          <Route path="/signup" element={<Signup />} />
-          <Route path="/login" element={<Login />} />
-        </Routes>
-      </Router>
+        {!isLoggedIn ? (
+        <ul>
+          <li>
+            <NavLink to="/signup" activeClassName="active">Sign Up</NavLink>
+          </li>
+          <li>
+            <NavLink to="/login" activeClassName="active">Login</NavLink>
+          </li>
+          <li>
+            <button onClick={handleLogin}>Simulate Login</button>
+          </li>
+        </ul>
+        ) : (
+        <ul>
+          <li>
+          <button onClick={handleLogout}>Logout</button>
+          </li>
+        </ul>
+        )}
+      </nav>
+      
     </div>
   )
 }
--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,11 +1,27 @@
 import React from "react";
-import Header from "../components/header/header.jsx";
 import "./App.css";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import Header from "../components/header/header.jsx";
+import Home from "../pages/home.jsx";
+import Products from "../pages/products.jsx";
+import About from "../pages/about.jsx";
+import Signup from "../pages/signup.jsx";
+import Login from "../pages/login.jsx";
 
 function App() {
   return (
     <>
-      <Header />
+      <Router>
+        <Header />
+
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/products" element={<Products />} />
+          <Route path="/about" element={<About />} />
+          <Route path="/signup" element={<Signup />} />
+          <Route path="/login" element={<Login />} />
+        </Routes>
+      </Router>
     </>
   );
 }
